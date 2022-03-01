5bd27287aeaee6547219b36b56581e4e328f977b
Alexei <alexei.prudnikov@gmail.com>
2022-03-01 06:08:11 +0300

    Add Task 13

---
+++ Project02/Program.cs
@@ -0,0 +1,26 @@
+﻿
+Console.WriteLine("=======Задача № 13=======");
+string Get3Digit (int number)
+{
+if (Math.Abs(number) < 100)
+{
+    return "Третьей цифры нет!";
+}
+else
+{
+    return number + " -> " + Math.Abs(number).ToString()[2].ToString();
+}
+
+}
+try
+{
+    Console.Write("Введите число:");
+    int number = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(Get3Digit(number));
+
+
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
