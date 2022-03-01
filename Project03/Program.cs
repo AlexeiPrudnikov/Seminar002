a2ca138c730b781ed63dcbbb90e3c7e8eca93136
Alexei <alexei.prudnikov@gmail.com>
2022-03-01 06:21:28 +0300

    Add Task 15

---
+++ Project03/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("=======Задача № 15=======");
+string IsWeekend (int dayNumber)
+{
+    if ((dayNumber == 6) | (dayNumber == 7))
+    {
+        return dayNumber + " -> да";
+    }
+    if ((dayNumber >= 1) & (dayNumber <= 5))
+    {
+        return dayNumber + " -> нет";
+    }
+    else
+    {
+        return "Дня недели с таким номером не сущществует";
+    }
+}
+try
+{
+    Console.Write("Введите номер дня недели:");
+    int dayNumber = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(IsWeekend(dayNumber));
+
+
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
