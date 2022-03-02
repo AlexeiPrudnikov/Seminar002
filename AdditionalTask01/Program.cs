1c3699df1af9edb108a96f514e29d7d4db93a390
Alexei <alexei.prudnikov@gmail.com>
2022-03-02 20:19:02 +0300

    Add AditionalTask1

--- AdditionalTask01/Program.cs
+++ AdditionalTask01/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("=======Дополнительная задача № 1=======");
+bool IsValidTriangle (uint[] sides)
+{
+    if (sides.Length != 3) return false;
+    if ((sides[0] + sides[1] <= sides[2])|(sides[0] + sides[2] <= sides[1])|(sides[1] + sides[2] <= sides[0])|((sides[0] * sides[1] * sides[2]) == 0))
+    {
+        return false;
+    }
+    return true;
+}
+string GetTypeTriangle(uint[] sides)
+{
+    if (IsValidTriangle(sides))
+    {
+        if ((sides[0] == sides[1]) & (sides[1] == sides[2]))
+        {
+            return "Треугольник является равносторонним";
+        }
+        else
+        {
+            if ((sides[0] == sides[1]) | (sides[1] == sides[2]) | (sides[0] == sides[2]))
+            {
+                return "Треугольник является равнобедренным";
+            }
+            else
+            {
+                return "Треугольник является обычным";
+            }
+        }
+
+    }
+    else
+    {
+        return "Треугольника с заданными сторонами не существует";
+    }
+}
+try
+{
+    uint[] sides = new uint[3];
+    for (int i = 0; i < 3; i++)
+    {
+        Console.Write("Введите длину " + (i + 1) + "-й стороны треугольника:");
+        sides[i] = Convert.ToUInt32(Console.ReadLine());
+    }
+    Console.WriteLine(GetTypeTriangle(sides));
+
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
