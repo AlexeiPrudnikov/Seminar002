1cc4524a4b6763d04fd6b7b1e62d49f816a838d6
Alexei <alexei.prudnikov@gmail.com>
2022-03-02 21:24:45 +0300

    Error TimeSpan

--- AdditionalTask02/Program.cs
+++ AdditionalTask02/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("=======Дополнительная задача № 2=======");
+string GetAge(DateTime interestDate, DateTime birthday)
+{
+    TimeSpan period = interestDate - birthday;
+    int days = Convert.ToInt32((interestDate - birthday).TotalDays);
+    Console.WriteLine(days);
+    string result = "";
+    DateTime age = new DateTime();
+    age.AddDays(days);
+    result = "На " + interestDate.ToShortDateString() + " человеку, рожденному " + birthday.ToShortDateString() + " -> " +
+             age.Year + " лет ";
+    return result;
+}
+try
+{
+    DateTime birthday;
+    Console.Write("Введите дату рождения:");
+    birthday = Convert.ToDateTime(Console.ReadLine());
+    Console.WriteLine(GetAge(Convert.ToDateTime("01.02.2022"), birthday));
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
