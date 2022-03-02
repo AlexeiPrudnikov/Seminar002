43c48fd489389864970acfe8185db94eeb266f8f
Alexei <alexei.prudnikov@gmail.com>
2022-03-02 21:37:50 +0300

    Add AdditionalTask03

--- AdditionalTask03/Program.cs
+++ AdditionalTask03/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("=======Дополнительная задача № 3=======");
+double GetIncome(double startMoney, int month)
+{
+    double money = startMoney;
+    for (int i = 0; i < month; i++)
+    {
+        money *= 1.015;
+    }
+    return money;
+}
+
+try
+{
+    Console.Write("Введите скрок вклада в месяцах: ");
+    int month = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Размер депозита через " + month + " месяцев составляет " +  GetIncome(1000, month));
+
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
